30d754c0e3f25fbab2f9ea1ac670e9aa6cfe1b40
Hamish <hamish.forbes@gmail.com>
2021-03-30 14:28:44 +1300

    Initial commit

---
+++ test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import datetime
+lens = [10, 100, 1*1024, 10*1024, 100*1024]
+for i in lens:
+    print(f'{datetime.datetime.now()} - Message length: {i} - {"a"*i}')
+for i in reversed(lens):
+    print(f'{datetime.datetime.now()} - Message length: {i} - {"a"*i}')
