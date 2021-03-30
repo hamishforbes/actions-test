0bf5fbbbc646e5aee6affc27768a6be77960f61f
Hamish <hamish.forbes@gmail.com>
2021-03-30 14:32:21 +1300

    Adjust message sizes

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import datetime
-lens = [10, 100, 1*1024, 10*1024, 100*1024]
+lens = [10, 100, 1*1024, 2*1024, 4*1024]
 for i in lens:
     print(f'{datetime.datetime.now()} - Message length: {i} - {"a"*i}')
 for i in reversed(lens):
